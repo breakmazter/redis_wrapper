e1ea9c2703d5f8b963c79d2374a0f49bb375205d
josep <iosif.mazan@krok.ai>
2023-02-13 15:39:59 +0300

    update: minor update

--- test_wrapper.py
+++ test_wrapper.py
@@ -1,8 +1,8 @@
-from redis_wrapper.RedisHook import RedisHook
+from RedisWrapper import RedisWrapper
 
-redis_connection = RedisHook(host='35.235.115.86', port=6379, password='TumbaPumba2093bX4KDxuRC6mALoLo')
+redis_connection = RedisWrapper(host='127.0.0.1', port=6379, password='TumbaPumba2093bX4KDxuRC6mALoLo')
 
 if __name__ == "__main__":
-  redis_connection.set_value(key=12, value="hello world")
-  data = redis_connection.get_value(key=12)
-  print(data)
+    redis_connection.set_value(key='12', value="hello world")
+    data = redis_connection.get_value(key='12')
+    print(data)
